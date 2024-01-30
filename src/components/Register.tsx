7dc64c43b062fb30f39df3292185cb8563390253
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-01-30 23:40:08 +0500

    POST by fetch

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,32 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+import { data } from "autoprefixer";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+  });
+  const BASE_URL = "http://127.0.0.1:8000/";
+
+  const handleFormSubmit = () => {
+    fetch(`${BASE_URL}/auth/users/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
       <div className="flex flex-col items-center w-full h-full pt-20 space-y-10">
@@ -33,7 +58,7 @@ const Register = () => {
           label="Password"
           variant="outlined"
         />
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" onClick={handleFormSubmit}>Submit</Button>
       </div>
     </>
   );
