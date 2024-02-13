eb1fef5b833b5a1dd136b12f30bb590e412e4e91
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-02-13 21:46:02 +0500

    errored

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Box, LinearProgress, List } from "@mui/material";
+import UserItems from "./UserItems";
 
 const Sidebar = () => {
   const BASE_URL = "http://127.0.0.1:8000/";
-  const [userList, setUserList] = useState({});
+  const [userList, setUserList] = useState([]) ;
+  const [userLoader, setUserLoader] = useState(true)
 
   const getAuthTokenFromCookie = () => {
     const [name, value] = document.cookie.split("=");
@@ -29,7 +32,9 @@ const Sidebar = () => {
         })
         .then((response) => {
           setUserList(response.data);
-          console.log(userList);          
+          setUserLoader(false)
+          // console.log(userList);  
+          // console.log(response.data);               
         })
         .catch((error) => {
           console.log("Error while making API request", error);
@@ -37,7 +42,14 @@ const Sidebar = () => {
     }
   }, []);
 
-  return <div className="sidebar"></div>;
+  return <div className="sidebar">
+  {userLoader ? (<Box sx={{width: 100%}}><LinearProgress /></Box> ): 
+  (<List sx={{width:'100%',maxWidth:360,bgcolor:'background.paper'}}>
+     {userList.map((user,index)=>{
+      <UserItems email={user.email} name={user.name}/>
+     })}
+  </List>)}
+  </div>
 };
 
 export default Sidebar;
