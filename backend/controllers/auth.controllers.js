c93cef23eac85c95bd9b73fb79e7a331821ef1d0
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-03-02 00:53:20 +0500

    Login setup completed

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -54,7 +54,28 @@ export const signup = async (req,res)=>{
 }
 
 export const login = async (req,res)=>{
-   
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || " ")
+
+        if (!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id:username._id,
+            fullName:user.fullName,
+            username:user.username,
+            profilePic:user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        return res.status(501).json({error:"Internal Server Error"})
+    }
 }
 
 export const logout = (req,res)=>{
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,7 +5,7 @@ import { signup,login,logout } from '../controllers/auth.controllers.js'
 const router = express.Router()
 
 router.post("/signup",signup)
-router.get("/login",login)
+router.post("/login",login)
 router.get("/logout", logout)
 
 export default router;
