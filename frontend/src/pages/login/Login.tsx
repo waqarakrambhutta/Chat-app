f654c9535e622a53588ef03ca20bf3b4a5b566c1
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-03-10 22:46:02 +0500

    Frontend: Using tailwing and daisyUI. Creating Login page.

---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,26 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          login
+          <span className="text-blue-500">Chatapp</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
