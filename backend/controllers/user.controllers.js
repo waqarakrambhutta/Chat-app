52e339d5251fc0435601cb9b77a61a2ab5ad79dc
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-03-10 16:25:02 +0500

    We can get all the users for sidebar

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,10 +1,12 @@
-export default getUsersForSidebar = () =>{
-    try {
-        
-    } catch (error) {
-        console.error("Error in getUserForSidebar controller ",error.message)
-        return res.status(500).json({error:"Internal Server Error"})
-    }
-}
+import User from "../models/user.model.js";
 
-
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    return res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUserForSidebar controller ", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import portectRoute from "../middleware/protectRoute.js";
-import getUsersForSidebar from "../controllers/user.controllers.js";
+import { getUsersForSidebar } from "../controllers/user.controllers.js";
+
 
 const router = express.Router();
 
