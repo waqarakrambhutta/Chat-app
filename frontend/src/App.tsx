ea1f677e27fef6887a75152f38918c0bbd872beb
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-03-14 15:09:48 +0500

    Routing with React-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
-  return <div className="p-4 h-screen flex items-center justify-center">
-    <Home/>
-  </div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/pages/signup/SignUp.tsx
+++ frontend/src/pages/signup/SignUp.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import GenderCheckBox from "./GenderCheckBox";
 
 const SignUp = () => {
@@ -5,8 +6,8 @@ const SignUp = () => {
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Signup
-          <span className="text-blue-500">Chatapp</span>
+          Sign Up
+          <span className="text-blue-500"> Chatapp</span>
         </h1>
         <form>
           <div>
@@ -52,12 +53,12 @@ const SignUp = () => {
 
            <GenderCheckBox/>
 
-          <a
-            href="#"
+          <Link
+            to={"/login"}
             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             Already have an account
-          </a>
+          </Link>
           <div>
             <button className="btn btn-block btn-sm mt-2">Login</button>
           </div>
@@ -68,77 +69,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-
-
-// STARTER CODE FOR SIGNUP PAGE.
-// import GenderCheckBox from "./GenderCheckBox";
-
-// const SignUp = () => {
-//   return (
-//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-//         <h1 className="text-3xl font-semibold text-center text-gray-300">
-//           Signup
-//           <span className="text-blue-500">Chatapp</span>
-//         </h1>
-//         <form>
-//           <div>
-//             <label className="label p-2">
-//               <span className="text-base label-text">Full Name</span>
-//             </label>
-//             <input
-//               type="text"
-//               placeholder="Waqar Akram"
-//               className="input input-bordered w-full h-10"
-//             />
-//           </div>
-//           <div>
-//             <label className="label p-2">
-//               <span className="text-base label-text">Username</span>
-//             </label>
-//             <input
-//               type="text"
-//               placeholder="waqarakram"
-//               className="input input-bordered w-full h-10"
-//             />
-//           </div>
-//           <div>
-//             <label className="label p-2">
-//               <span className="text-base label-text">Password</span>
-//             </label>
-//             <input
-//               type="password"
-//               placeholder="Enter Password"
-//               className="input input-bordered w-full h-10"
-//             />
-//           </div>
-//           <div>
-//             <label className="label p-2">
-//               <span className="text-base label-text">Confirm Password</span>
-//             </label>
-//             <input
-//               type="password"
-//               placeholder="Confirm Password"
-//               className="input input-bordered w-full h-10"
-//             />
-//           </div>
-
-//            <GenderCheckBox/>
-
-//           <a
-//             href="#"
-//             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
-//           >
-//             Already have an account
-//           </a>
-//           <div>
-//             <button className="btn btn-block btn-sm mt-2">Login</button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignUp;
