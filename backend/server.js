c0d5740a199fa5f5f95f5242bbbb1be327627957
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-03-25 01:35:06 +0500

    for streek

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.routes.js"
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
